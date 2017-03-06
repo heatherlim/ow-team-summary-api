1d0504fc7a2dbc4c83a3648b7baf27c2ccd419cb
github username <hhl262@nyu.edu>
2017-03-06 04:33:51 -0500

    some formatting on scraper

--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,31 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 class Scraper
   attr_accessor :doc
+  
   def initialize
-    @doc = Nokogiri::HTML(open('https://playoverwatch.com/en-us/career/pc/us/#{bnettaghere}'))
+    @doc = Nokogiri::HTML(open('https://playoverwatch.com/en-us/career/pc/us/happysquared-1920'))
   end
   
   def competitive_rank
     @doc.css('.competitive-rank .h6').children.first.text
   end
   
-  def top_three_heroes
-    first_hero = @doc.css('#competitive .bar-text .title')[0].text
-    first_hero_time = @doc.css('#competitive .bar-text .description')[0].text
-    
-    second_hero = @doc.css('#competitive .bar-text .title')[1].text
-    second_hero_time = @doc.css('#competitive .bar-text .description')[1].text
-    
-    third_hero = @doc.css('#competitive .bar-text .title')[2].text
-    third_hero_time = @doc.css('#competitive .bar-text .description')[2].text
+  def top_three_heroes_played
+    {
+      @doc.css('#competitive .bar-text .title')[0].text => @doc.css('#competitive .bar-text .description')[0].text,
+      @doc.css('#competitive .bar-text .title')[1].text => @doc.css('#competitive .bar-text .description')[1].text,
+      @doc.css('#competitive .bar-text .title')[2].text => @doc.css('#competitive .bar-text .description')[2].text
+    }
+  end
+  
+  def player_name
+    battletag = @doc.css('.header-masthead').text
+  end
+  
+  def player_portrait
+    portrait = @doc.css('.masthead .masthead-player .player-portrait').attr('src').value
   end
   
 end
